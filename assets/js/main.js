cead28f414dce62e42d4f22814efec21c59121a6
pettik <petrbednarski@gmail.com>
2023-12-20 20:13:08 +0100

    final updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,99 +5,97 @@ const products = [
     name: 'Sněhová koule',
     'src-img': 'assets/img/accessory1.png',
     price: 94.5,
-    stock:4,
+    stock: 4,
     description: 'Zábavné těžítko',
   },
   {
     name: 'Vánoční lízátko',
     'src-img': 'assets/img/accessory2.png',
     price: 15.4,
-    stock:10,
+    stock: 10,
     description: 'Vánoční dobroty',
   },
   {
     name: 'Andělíček',
     'src-img': 'assets/img/accessory3.png',
     price: 34.2,
-    stock:18,
+    stock: 18,
     description: 'Vánoční dekorace',
   },
   {
     name: 'Červená baňka',
     'src-img': 'assets/img/accessory4.png',
     price: 12.5,
-    stock:37,
+    stock: 37,
     description: 'Přináší štěstí',
   },
   {
-    name: 'Vánoční překvapení',
+    name: 'Vánoční dárek',
     'src-img': 'assets/img/accessory5.png',
     price: 99.99,
-    stock:99,
+    stock: 99,
     description: 'Překvapení uvnitř',
   },
   {
     name: 'Zelená baňka',
     'src-img': 'assets/img/accessory6.png',
-    price: 15.20,
-    stock:85,
+    price: 15.2,
+    stock: 85,
     description: 'Vánoční dekorace',
   },
   {
     name: 'Vánoční stromek',
     'src-img': 'assets/img/accessory7.png',
     price: 299.5,
-    stock:22,
+    stock: 22,
     description: 'Blikající stormeček',
   },
   {
     name: 'Santova čepice',
     'src-img': 'assets/img/accessory8.png',
     price: 99.99,
-    stock:4,
+    stock: 4,
     description: 'Zateplená čepice',
   },
   {
     name: 'Vánoční cukroví',
     'src-img': 'assets/img/accessory9.png',
-    price: 180.00,
-    stock:970,
+    price: 180.0,
+    stock: 970,
     description: 'Cukroví (cena za 1kg)',
   },
   {
     name: 'Santův sob',
     'src-img': 'assets/img/accessory10.png',
-    price: 99449.20,
-    stock:13,
+    price: 99449.2,
+    stock: 13,
     description: 'Obsahuje dáreček',
   },
   {
     name: 'Zvonky štěstí',
     'src-img': 'assets/img/accessory11.png',
     price: 299.99,
-    stock:18,
+    stock: 18,
     description: 'Zazvoň pro Vánoce',
   },
   {
     name: 'Vánoční věnec',
     'src-img': 'assets/img/accessory12.png',
     price: 174.1,
-    stock:2,
+    stock: 2,
     description: 'Vánoční dekorace',
   },
   {
     name: 'Sněžné sáně',
     'src-img': 'assets/img/accessory13.png',
     price: 0.99,
-    stock:150,
+    stock: 150,
     description: 'AKCE - není sníh',
-  }
+  },
 ];
 
-// <!--=============== SWIPER JS JS ===============-->
-
+/*=============== GENEROVÁNÍ PRODUKTŮ Z POLE PRODCUTS ===============*/
 document.addEventListener('DOMContentLoaded', function () {
-  // Funkce pro generování produktů
   function generateProducts() {
     const productsContainer = document.querySelector(
       '.accessory__container.swiper-wrapper'
@@ -107,15 +105,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     products.forEach(function (item) {
       // Formátování ceny na dvě desetinná místa
-      let originalprice = (item.price*1.3).toLocaleString(undefined, {
+      let originalprice = (item.price * 1.3).toLocaleString(undefined, {
         minimumFractionDigits: 2,
-        maximumFractionDigits: 2
+        maximumFractionDigits: 2,
       });
       let displayedPrice = item.price.toLocaleString(undefined, {
         minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      })
-
+        maximumFractionDigits: 2,
+      });
 
       const productDiv = document.createElement('div');
       productDiv.className = 'swiper-slide accessory__content';
@@ -143,12 +140,13 @@ document.addEventListener('DOMContentLoaded', function () {
   // Spustit generování produktů po načtení DOM
   generateProducts();
 
+  /*=============== SWIPER JS===============*/
   var swiper = new Swiper('.mySwiper', {
     effect: 'cards',
     grabCursor: true,
   });
 
-  // Nastavení hudby
+  /*=============== TLAČÍTKO HUDBY ===============*/
   var backgroundMusic = document.getElementById('background-music');
   backgroundMusic.volume = 0.3; // Nastavení hlasitosti na 30%
 
@@ -177,6 +175,30 @@ document.addEventListener('DOMContentLoaded', function () {
   navVolume.addEventListener('click', function () {
     toggleMusic();
   });
+
+  /*=============== MAIN OVERLAY SHOW BOX ===============*/
+  const overlay = document.querySelector('.main__overlay');
+  const overlayBox = document.querySelector('.main__overlay__article');
+  const closeBtn = document.querySelector('#main-close-btn');
+  const shoppingCartButtons = document.querySelectorAll('.accessory__button');
+
+  shoppingCartButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      overlay.classList.add('show-overlay');
+      overlayBox.classList.add('show-overlay');
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    overlay.classList.remove('show-overlay');
+    overlayBox.classList.remove('show-overlay');
+  });
+
+  overlay.addEventListener('click', () => {
+    overlay.classList.remove('show-overlay');
+    overlayBox.classList.remove('show-overlay');
+  });
+
   //   konec DOMContentLoaded
 });
 
@@ -186,7 +208,6 @@ const darkTheme = 'dark-theme';
 const iconTheme = 'ri-moon-line';
 const iconTheme1 = document.querySelector('#theme-icon1');
 const iconTheme2 = document.querySelector('#theme-icon2');
-
 
 // Previously selected topic (if user selected)
 const selectedTheme = localStorage.getItem('selected-theme');
@@ -211,19 +232,19 @@ if (selectedTheme) {
   // );
 }
 
-const replaceIcons = ()=>{
-  if(iconTheme1.classList.contains('ri-lightbulb-line')){
+const replaceIcons = () => {
+  if (iconTheme1.classList.contains('ri-lightbulb-line')) {
     iconTheme1.classList.add('ri-lightbulb-line');
     iconTheme1.classList.remove('ri-moon-line');
     iconTheme2.classList.add('ri-moon-line');
     iconTheme2.classList.remove('ri-lightbulb-line');
-  }else{
+  } else {
     iconTheme1.classList.add('ri-moon-line');
     iconTheme1.classList.remove('ri-lightbulb-line');
     iconTheme2.classList.add('ri-lightbulb-line');
     iconTheme2.classList.remove('ri-moon-line');
   }
-}
+};
 
 // Activate / deactivate the theme manually with the button
 themeButton.addEventListener('click', () => {
@@ -231,7 +252,7 @@ themeButton.addEventListener('click', () => {
   document.body.classList.toggle(darkTheme);
   // themeButton.classList.toggle(iconTheme);
   replaceIcons();
-  
+
   // We save the theme and the current icon that the user chose
   localStorage.setItem('selected-theme', getCurrentTheme());
   localStorage.setItem('selected-icon', getCurrentIcon());
